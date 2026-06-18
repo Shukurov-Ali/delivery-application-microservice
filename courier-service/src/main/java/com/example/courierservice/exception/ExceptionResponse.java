4fc26073a8857a06975c710d21b0a43ce9e816dd
alishukurovdev <snin_GitHub_email_adresin>
2026-06-18 19:56:04 +0400

    feat(courier-service): add exception handling and error responses

---
+++ courier-service/src/main/java/com/example/courierservice/exception/ExceptionResponse.java
@@ -0,0 +1,16 @@
+package com.example.courierservice.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExceptionResponse {
+    private String code;
+    private String message;
+
+    public ExceptionResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
