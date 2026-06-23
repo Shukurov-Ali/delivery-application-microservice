bcec508031bd7053d8644c1ccdbe6a29d7879099
alishukurovdev <snin_GitHub_email_adresin>
2026-06-23 13:00:42 +0400

    fix(courier-service): add validation exception handling

--- courier-service/src/main/java/com/example/courierservice/constants/ExceptionsCodes.java
+++ courier-service/src/main/java/com/example/courierservice/constants/ExceptionsCodes.java
@@ -3,4 +3,5 @@ package com.example.courierservice.constants;
 public interface ExceptionsCodes {
     String COURIER_NOT_FOUND_CODE = "courier.not.found";
     String COURIER_ALREADY_EXISTS_CODE = "courier.already.exists";
+    String VALIDATION_ERROR_CODE = "validation.error";
 }
--- courier-service/src/main/java/com/example/courierservice/controller/GlobalExceptionHandler.java
+++ courier-service/src/main/java/com/example/courierservice/controller/GlobalExceptionHandler.java
@@ -4,12 +4,12 @@ import com.example.courierservice.exception.CourierAlreadyExistsException;
 import com.example.courierservice.exception.CourierNotFoundException;
 import com.example.courierservice.exception.ExceptionResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static com.example.courierservice.constants.ExceptionsCodes.COURIER_ALREADY_EXISTS_CODE;
-import static com.example.courierservice.constants.ExceptionsCodes.COURIER_NOT_FOUND_CODE;
+import static com.example.courierservice.constants.ExceptionsCodes.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -24,6 +24,20 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ExceptionResponse handleException(CourierAlreadyExistsException exception) {
         return new ExceptionResponse(COURIER_ALREADY_EXISTS_CODE, exception.getMessage());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleValidationException(MethodArgumentNotValidException exception) {
+
+        String message = exception.getBindingResult()
+                .getFieldErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        return new ExceptionResponse(
+                VALIDATION_ERROR_CODE,
+                message
+        );
     }
 
 }
